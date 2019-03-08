acdb2a27a8562faab5f8513fb44c202b6e6501ca
clone-sniper <clone-sniper@clone-sniper-PC>
2019-03-07 17:31:33 -0800

    testing

--- GroupProject/src/groupproject/GroupProject.java
+++ GroupProject/src/groupproject/GroupProject.java
@@ -5,6 +5,10 @@
  */
 package groupproject;
 
+import Model.Customer;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 /**
  *
  * @author Eric Petersen
@@ -15,7 +19,16 @@ public class GroupProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Persist");
+        EntityManager em = emf.createEntityManager();
+        
+        Customer cust = em.find(Customer.class, 1);
+        System.out.println(cust.getName());
+        cust.setName("Bob");
+        System.out.println(cust.getName());
+        Customer cust2 = em.find(Customer.class, 2);
+        System.out.println(cust2.getEmail());
+        System.out.println(cust2.toString());
     }
     
 }
