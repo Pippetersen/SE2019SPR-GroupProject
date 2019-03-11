743179b992f1414e3c9a2e728d7c20f9aad6515f
Pippetersen <48065929+Pippetersen@users.noreply.github.com>
2019-03-11 10:54:19 -0700

    Update MainMenu.java

--- GroupProject/src/Menu/MainMenu.java
+++ GroupProject/src/Menu/MainMenu.java
@@ -21,7 +21,8 @@ public final class MainMenu implements MenuInterface {
         DisplayCustomerListMenu displayCustMenu;
         QuitMenu quitMenu;
         List<MenuInterface> menuList;
-    
+    //This is an example
+        
     //Constructor initializes each item
     public MainMenu() {
         menuList = new ArrayList<>();
@@ -29,6 +30,7 @@ public final class MainMenu implements MenuInterface {
         delCustMenu = new DeleteCustomerMenu();
         displayCustMenu = new DisplayCustomerListMenu();
         quitMenu = new QuitMenu();
+        int a = 0;
         //Always have at least the quit menu on the menu
         addToMainMenu(quitMenu);
         //Add the menu items for the first homework assignment
