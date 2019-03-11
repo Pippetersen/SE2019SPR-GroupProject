4e5c53fb903fd74d51eeb98d29bd75bab1cfe915
vaicam <vaicam07@gmail.com>
2019-03-11 15:57:29 -0700

    created Controller Package

---
+++ GroupProject/src/Controller/Controller.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.Customer;
+import Model.CustomerDB;
+import Model.FileData;
+import Menu.MainMenu;
+import java.io.IOException;
+import java.util.ArrayList;
+/**
+ *
+ * @author Cameron Patterson
+ */
+public class Controller {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException, ClassNotFoundException
+    {
+
+        
+    }
+    
+}
